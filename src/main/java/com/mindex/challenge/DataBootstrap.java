dde2129a43eec2381933b1345e8986f84bb110bd
Abhinandan Desai <55063572+abhinandandesai@users.noreply.github.com>
2020-12-22 14:52:57 -0500

    Add files via upload

---
+++ src/main/java/com/mindex/challenge/DataBootstrap.java
@@ -0,0 +1,52 @@
+package com.mindex.challenge;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mindex.challenge.dao.CompensationRepository;
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.data.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class DataBootstrap {
+    private static final String DATASTORE_LOCATION = "/static/employee_database.json";
+    private static final String COMP_DATASTORE_LOCATION = "/static/compensations.json";
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private CompensationRepository compensationRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @PostConstruct
+    public void init() {
+        InputStream inputStream = this.getClass().getResourceAsStream(DATASTORE_LOCATION);
+        InputStream compensationsInStream = this.getClass().getResourceAsStream(COMP_DATASTORE_LOCATION);
+
+        Employee[] employees = null;
+        Compensation[] compensations = null;
+
+        try {
+            employees = objectMapper.readValue(inputStream, Employee[].class);
+            compensations = objectMapper.readValue(compensationsInStream, Compensation[].class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (Employee employee : employees) {
+            employeeRepository.insert(employee);
+        }
+
+        for (Compensation compensation : compensations) {
+            compensationRepository.insert(compensation);
+        }
+    }
+}
